d36896d41b47f90b055f0cc1058f28927474100b
Sonal-21200 <kandarisonal21200@gamil.com>
2024-03-14 13:34:38 +0530

    conflicts resolved

--- frontend/src/features/cart/cartAPI.js
+++ frontend/src/features/cart/cartAPI.js
@@ -5,41 +5,26 @@ export function addToCart(item) {
       body: JSON.stringify(item),
       headers: { 'content-type': 'application/json' },
     });
+    console.log(JSON.stringify(item));
     const data = await response.json();
+    console.log(response)
     // TODO: on server it will only return some info of user (not password)
     resolve({ data });
   });
 }
 
 export function fetchItemsByUserId(userId) {
-  return new Promise(async (resolve) =>{
-<<<<<<< HEAD
-   const response = await fetch('http://localhost:8081/cart', {
-    method: 'POST',
-    body : JSON.stringify(item),
-    headers: {'content-type' : 'application/json'},
-   })
-   const data = await response.json()
-   resolve({data})
-   }
-  );
-}
-
-export function fetchItemsByUserId(userId) {
   return new Promise(async (resolve) => {
-    const response = await fetch ('http://localhost:8081/cart?user='+ userId)
-=======
     //TODO: we will not hard-code server URL here
-    const response = await fetch('http://localhost:8081/cart?user='+userId) 
->>>>>>> iqra
-    const data = await response.json()
-    resolve({data})
-  })
+    const response = await fetch('http://localhost:8081/cart?users=' + userId);
+    const data = await response.json();
+    resolve({ data });
+  });
 }
 
 export function updateCart(update) {
   return new Promise(async (resolve) => {
-    const response = await fetch('http://localhost:8081/cart/'+update.id, {
+    const response = await fetch('http://localhost:8081/cart/' + update.id, {
       method: 'PATCH',
       body: JSON.stringify(update),
       headers: { 'content-type': 'application/json' },
@@ -50,15 +35,26 @@ export function updateCart(update) {
   });
 }
 
-
 export function deleteItemFromCart(itemId) {
   return new Promise(async (resolve) => {
-    const response = await fetch('http://localhost:8081/cart/'+itemId, {
+    const response = await fetch('http://localhost:8081/cart/' + itemId, {
       method: 'DELETE',
       headers: { 'content-type': 'application/json' },
     });
     const data = await response.json();
     // TODO: on server it will only return some info of user (not password)
-    resolve({ data:{id:itemId} });
+    resolve({ data: { id: itemId } });
+  });
+}
+
+export function resetCart(userId) {
+  // get all items of user's cart - and then delete each
+  return new Promise(async (resolve) => {
+    const response = await fetchItemsByUserId(userId);
+    const items = response.data;
+    for (let item of items) {
+      await deleteItemFromCart(item.id);
+    }
+    resolve({status:'success'})
   });
 }
